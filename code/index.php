cbbbc347d88b3feefb810d9d4bbf80ee4183f1d1
Tsvetkov Maxim <pushiniiist@gmail.com>
2024-03-01 08:13:44 +0200

    Task 2 complete; I hope I'll not get expelled from this university for such variable namings

--- code/index.php
+++ code/index.php
@@ -7,5 +7,33 @@ $very_bad_unclear_name = "15 chicken wings";
 $order =& $very_bad_unclear_name;
 $order .= " and let's praise the Kotyonok Ra!";
 
+// Today is 1'st of March, so let's make our integer variable like this date!
+$NumberOfThisDay = 1;
+echo $NumberOfThisDay;
+/* Actually, the Task 1 asked me to use "\n" to jump on a next line,
+but if I get everything right, it only works in console output, so I would like to add <br> HTML tag to make it
+look like the same in browser too.
+*/
+echo "\n";
+echo "<br>";
+
+// TBH I'm really sorry for this name, but it seemed to me really, funny and cool despite the fact it's not.
+$NumberThatWasGoingToBeThePiNumberButWeHadToChangeItToAnotherToMakeTwelveGreatAgain = 6.5;
+
+echo $NumberThatWasGoingToBeThePiNumberButWeHadToChangeItToAnotherToMakeTwelveGreatAgain +
+    $NumberThatWasGoingToBeThePiNumberButWeHadToChangeItToAnotherToMakeTwelveGreatAgain -
+    $NumberOfThisDay;
+echo "\n";
+echo "<br>";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+$difference = $last_month - $this_month;
+// Базированная жизнь студента, кстати :)
+echo "Я потратил в этом месяце на $difference рубля меньше, чем в предыдущем.";
+
+echo "\n";
+echo "<br>";
+
 // Don't change the line below
-echo "\nYour order is: $very_bad_unclear_name.";
+echo "\nYour order is: $very_bad_unclear_name";
