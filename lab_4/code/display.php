a5bef4c9d33d73b54b440113753b2e0f833fa4bb
Maxim Tsvetkov <pushiniiist@gmail.com>
2024-06-03 16:45:51 +0300

    DONE

--- lab_4/code/display.php
+++ lab_4/code/display.php
@@ -1,46 +1,23 @@
 <?php
 require "vendor/autoload.php";
 
+$mysqli = new mysqli("db", "root", "helloworld", "web");
 
-$client = new \Google_Client();
-$client->setApplicationName('AVITO ONLINE announcements');
-$client->setScopes(['https://www.googleapis.com/auth/spreadsheets']);
-$client->setAccessType('offline');
-$path = 'credentials.json';
-$client->setAuthConfig($path);
-
-// configure the Sheets Service
-$service = new \Google_Service_Sheets($client);
-
-$spreadsheetId = '1YjPHk_M1yt4rDjwSC2Gw22Fbw64JRllw2M-LvZPgP38';
-
-
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+}
 
 if ('POST' === $_SERVER['REQUEST_METHOD']) {
-    $email = $_POST['email'] ?? '';
-    $title = $_POST['title'] ?? '';
-    $category = $_POST['categories'] ?? '';
-    $description = $_POST['text'] ?? '';
+    $email = $mysqli->real_escape_string($_POST['email']);
+    $title = $mysqli->real_escape_string($_POST['title']);
+    $category = $mysqli->real_escape_string($_POST['categories']);;
+    $description = $mysqli->real_escape_string($_POST['text']);
 
-        // Данные для записи
-        $values = [
-            [$email, $title, $category, $description]
-        ];
-    }
+    $query = "INSERT INTO ad (email,title,category,description) VALUES ('$email','$title','$category','$description')";
+    $mysqli->query($query);
+}
 
-    // Определение диапазона для записи
-    $range = 'Лист1';
 
-    // Записываем данные в таблицу
-    $body = new Google_Service_Sheets_ValueRange([
-        'values' => $values
-    ]);
-    $params = [
-        'valueInputOption' => 'RAW'
-    ];
-
-    // Выполняем запись данных
-    $result = $service->spreadsheets_values->append($spreadsheetId, $range, $body, $params);
 
 header('Location: index.php');
 exit();
--- lab_4/code/index.php
+++ lab_4/code/index.php
@@ -1,3 +1,23 @@
+<?php
+    $mysqli = new mysqli('db', 'root', 'helloworld', 'web');
+    if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit;
+    }
+
+
+    $arr = [];
+    if ($result = $mysqli->query("SELECT * FROM ad ORDER BY created DESC")) {
+    while ($row = $result->fetch_assoc()) {
+    array_push($arr, $row);
+    }
+    $result->close();
+    }
+    $mysqli->close();
+?>
+
+
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -45,44 +65,18 @@
 </thead>
     </div>
     <tbody>
-    <?php
-    require 'vendor/autoload.php';
-    $client = new \Google_Client();
-    $client->setApplicationName('AVITO ONLINE announcements');
-    $client->setScopes(['https://www.googleapis.com/auth/spreadsheets']);
-    $client->setAccessType('offline');
-    $path = 'credentials.json';
-    $client->setAuthConfig($path);
-    
-    $service = new \Google_Service_Sheets($client);
 
-    $spreadsheetId = '1YjPHk_M1yt4rDjwSC2Gw22Fbw64JRllw2M-LvZPgP38';
-    $spreadsheet = $service->spreadsheets->get($spreadsheetId);
-    $sheets = $spreadsheet->getSheets();
-
-
-    foreach ($sheets as $sheet) {
-        $sheetTitle = $sheet->getProperties()->getTitle();
-        $range = $sheetTitle;
-
-        $response = $service->spreadsheets_values->get($spreadsheetId, $range);
-        $values = $response->getValues();
-
-
-        if (!empty($values)) {
-            $isFirstRow = true; 
-            foreach ($values as $row) {
-                echo '<tr>';
-                foreach ($row as $cell) {
-                        echo '<td>' . htmlspecialchars($cell) . '</td>';
-                }
-                echo '</tr>';
-            }
-        }
-    }
+<?php
+    foreach ($arr as $ad):
+        echo "<tr>";
+        echo "<td>" . ($ad['email']) . "</td>";
+        echo "<td>" . ($ad['title']) . "</td>";
+        echo "<td>" . ($ad['description']) . "</td>";
+        echo "<td>" . ($ad['category']) . "</td>";
+        echo "</tr>";
+        endforeach;
 ?>
 </tbody>
-</table>
 </div>
 </body>
 </html>
